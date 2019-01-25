bfd86954f7ecbaca8219e2a76d8568ddc53244dc
vchen <vchen@localhost.localdomain>
2019-01-25 16:25:49 -0500

    java code

--- src/main/java/org/dcache/vfs4j/NfsMain.java
+++ src/main/java/org/dcache/vfs4j/NfsMain.java
@@ -31,90 +31,99 @@ public class NfsMain {
 
     public static void main(String[] args) throws Exception {
 
-        if (args.length != 2) {
-            System.err.println("Usage: NfsMain <path> <export file>");
-            System.exit(1);
-        }
-        
-        Properties props = new Properties();
-        
-        InputStream configInput = ClassLoader.getSystemClassLoader().getResourceAsStream("conf.properties");
-        	
-        props.load(configInput);
-        
+	if (args.length != 2) {
+	    System.err.println("Usage: NfsMain <path> <export file>");
+	    System.exit(1);
+	}
 
-//        IgniteConfiguration igniteConfiguration = new IgniteConfiguration(); 
-//
-//        DataStorageConfiguration dataStorageConfig = new DataStorageConfiguration(); 
-//
-//        long offHeapMemoryMax = 2L * 1024 * 1024 * 1024; 
-//
-//        DataRegionConfiguration dataRegionConfig = new DataRegionConfiguration(); 
-//        dataRegionConfig.setInitialSize((long) Math.ceil(0.25 * offHeapMemoryMax)); // 20% of 2GB
-//        dataRegionConfig.setMaxSize(offHeapMemoryMax); // 256MB, for testing purposes 
-//        
-//        dataRegionConfig.setPageEvictionMode(DataPageEvictionMode.RANDOM_LRU); 
-//        dataRegionConfig.setEvictionThreshold(0.7); 
-//        dataRegionConfig.setEmptyPagesPoolSize(10000); 
-//        dataRegionConfig.setName("2GB_Region"); 
-//        
-//        dataStorageConfig.setDataRegionConfigurations(dataRegionConfig); 
-//        igniteConfiguration.setDataStorageConfiguration(dataStorageConfig); 
-        
+	//1. Read conf from xml file, including all data storage, data region and cache configuration
+	Properties props = new Properties();
 
-        Ignite ignite = Ignition.start(props.getProperty("cache.ignite.conf"));
-        
-        //or load default from src/main/resources
-        // Ignite ignite = Ignition.start("default-config.xml");
-        
-        
-    //    CacheConfiguration config = new CacheConfiguration("content-access-cache");
-        
-        //set any config here before start cache
-        //config.setCacheMode(CacheMode.PARTITIONED);  //example
-        //config.setAtomicityMode(CacheAtomicityMode.ATOMIC);
-        //config.setBackups(0);
-     //   config.setDataRegionName("2GB_Region");
-        
-     //   igniteConfiguration.setCacheConfiguration(config); 
-        //*** 
-        
-     //   Ignite ignite = Ignition.start(igniteConfiguration); 
-       
+	InputStream configInput = ClassLoader.getSystemClassLoader().getResourceAsStream("conf.properties");
+
+	props.load(configInput);
+
+	Ignite ignite = Ignition.start(props.getProperty("cache.ignite.conf"));
+
+	//or load default from src/main/resources
+	// Ignite ignite = Ignition.start("default-config.xml");
+
+	
+	//comment this line out if you use cache configuration in the code
 	IgniteCache<String, byte[]> cache = ignite.getOrCreateCache("content-access-cache");
 
-        VirtualFileSystem vfs = new LocalVFS( new File(args[0]), cache, ignite);
-        OncRpcSvc nfsSvc = new OncRpcSvcBuilder()
-                .withPort(2049)
-                .withTCP()
-                .withAutoPublish()
-                .withWorkerThreadIoStrategy()
-                .build();
 
-        ExportFile exportFile = new ExportFile( new File(args[1]));
+	//***Or 2. Create conf in code***
+	//        IgniteConfiguration igniteConfiguration = new IgniteConfiguration(); 
+	//
+	//        DataStorageConfiguration dataStorageConfig = new DataStorageConfiguration(); 
+	//
+	//        long offHeapMemoryMax = 2L * 1024 * 1024 * 1024; 
+	//
+	//        DataRegionConfiguration dataRegionConfig = new DataRegionConfiguration(); 
+	//        dataRegionConfig.setInitialSize((long) Math.ceil(0.25 * offHeapMemoryMax)); // 20% of 2GB
+	//        dataRegionConfig.setMaxSize(offHeapMemoryMax); // 256MB, for testing purposes 
+	//        
+	//        dataRegionConfig.setPageEvictionMode(DataPageEvictionMode.RANDOM_LRU); 
+	//        dataRegionConfig.setEvictionThreshold(0.7); 
+	//        dataRegionConfig.setEmptyPagesPoolSize(10000); 
+	//        dataRegionConfig.setName("2GB_Region"); 
+	//        
+	//        dataStorageConfig.setDataRegionConfigurations(dataRegionConfig); 
+	//        igniteConfiguration.setDataStorageConfiguration(dataStorageConfig); 
 
-//        VfsCacheConfig cacheConfig = new VfsCacheConfig();
-//        cacheConfig.setMaxEntries(1000000);
-//        cacheConfig.setLifeTime(5);
-//        cacheConfig.setFsStatLifeTime(1);
-//        VfsCache vfsCache = new VfsCache(vfs, cacheConfig);
-        
-        NFSServerV41 nfs4 = new NFSServerV41.Builder()
-                .withExportFile(exportFile)
-                .withVfs(vfs)
-                .withOperationFactory(new MDSOperationFactory())
-                .build();
+	//Add this line if use Ignite Configuration in code
+	//   Ignite ignite = Ignition.start(igniteConfiguration); 
 
-        NfsServerV3 nfs3 = new NfsServerV3(exportFile, vfs);
-        MountServer mountd = new MountServer(exportFile, vfs);
 
-        nfsSvc.register(new OncRpcProgram(100003, 3), nfs3);
-        nfsSvc.register(new OncRpcProgram(100005, 3), mountd);
+	//*** create CacheConfiguration in code ****
+	//    CacheConfiguration config = new CacheConfiguration("content-access-cache");
 
-        nfsSvc.register(new OncRpcProgram(nfs4_prot.NFS4_PROGRAM, nfs4_prot.NFS_V4), nfs4);
-        nfsSvc.start();
+	//set any config here before start cache
+	//config.setCacheMode(CacheMode.PARTITIONED);  //example
+	//config.setAtomicityMode(CacheAtomicityMode.ATOMIC);
+	//config.setBackups(0);
+	//   config.setDataRegionName("2GB_Region");
 
-        Thread.currentThread().join();
+	//   igniteConfiguration.setCacheConfiguration(config); 
+
+	// IgniteCache<String, byte[]> cache = ignite.getOrCreateCache(config);
+
+
+
+	VirtualFileSystem vfs = new LocalVFS( new File(args[0]), cache, ignite);
+	OncRpcSvc nfsSvc = new OncRpcSvcBuilder()
+		.withPort(2049)
+		.withTCP()
+		.withAutoPublish()
+		.withWorkerThreadIoStrategy()
+		.build();
+
+	ExportFile exportFile = new ExportFile( new File(args[1]));
+
+	//        VfsCacheConfig cacheConfig = new VfsCacheConfig();
+	//        cacheConfig.setMaxEntries(1000000);
+	//        cacheConfig.setLifeTime(5);
+	//        cacheConfig.setFsStatLifeTime(1);
+	//        VfsCache vfsCache = new VfsCache(vfs, cacheConfig);
+
+	NFSServerV41 nfs4 = new NFSServerV41.Builder()
+		.withExportFile(exportFile)
+		.withVfs(vfs)
+		.withOperationFactory(new MDSOperationFactory())
+		.build();
+
+	NfsServerV3 nfs3 = new NfsServerV3(exportFile, vfs);
+	MountServer mountd = new MountServer(exportFile, vfs);
+
+	nfsSvc.register(new OncRpcProgram(100003, 3), nfs3);
+	nfsSvc.register(new OncRpcProgram(100005, 3), mountd);
+
+	nfsSvc.register(new OncRpcProgram(nfs4_prot.NFS4_PROGRAM, nfs4_prot.NFS_V4), nfs4);
+	nfsSvc.start();
+
+	Thread.currentThread().join();
     }
 }
+
 
